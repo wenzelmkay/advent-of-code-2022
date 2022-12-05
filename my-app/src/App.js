b74c85c9b358b04bd9071d3a93fc8a2cf01ca17f
Mia Wenzel <wenzelmk@Mias-MacBook-Pro.local>
2022-12-05 09:32:09 -0600

    adding a react app to the project

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AdventOfCode from "./days/AdventOfCode";
+import {Drawer} from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+        <AdventOfCode/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/days/AdventOfCode.js
@@ -0,0 +1,11 @@
+import './AdventOfCode.css';
+
+function AdventOfCode() {
+    return (
+        <div className="advent-of-code-content">
+            <p>We wish you a merry code-mas!!!</p>
+        </div>
+    );
+}
+
+export default AdventOfCode;
