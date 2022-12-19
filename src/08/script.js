4087196fa86070346819f2afc52da3b0abb28fa8
Mia Wenzel <wenzelmk@Mias-MacBook-Pro.local>
2022-12-19 17:01:15 -0600

    day 8

---
+++ src/08/script.js
@@ -0,0 +1,34 @@
+import {input} from "./input.js";
+import {inputPersonal} from "./inputPersonal.js";
+
+const partOne = (string) => {
+    const array = string.split("\n")
+    const array.forEach()
+
+}
+
+const partTwo = (array) => {
+}
+
+// 10
+const testCase = "30373\n" +
+    "25512\n" +
+    "65332\n" +
+    "33549\n" +
+    "35390"
+
+const run = (string) => {
+    console.log("Part One Value: ", partOne(string))
+    console.log("Part Two Value: ", partTwo(string))
+}
+
+console.log("--------")
+console.log("Test Case")
+run(testCase);
+console.log("--------")
+// console.log("Work Challenge")
+// run(input);
+// console.log("--------")
+// console.log("Personal Challenge")
+// run(inputPersonal);
+// console.log("--------")
